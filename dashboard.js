eebe7ee09090b8574302be529cd287b2e497556f
manasabingi115 <manasabingi115@gmail.com>
2021-11-04 15:29:23 +0530

    Added functionality to post task to API

--- dashboard.js
+++ dashboard.js
@@ -1,5 +1,7 @@
 var listItems = document.getElementById('listItems');
 
+var submit = document.getElementById('submit');
+
 fetch('https://api-nodejs-todolist.herokuapp.com/task', {
   headers: {
     'Content-Type': 'application/json',
@@ -19,3 +21,22 @@ fetch('https://api-nodejs-todolist.herokuapp.com/task', {
 
     console.log(newList);
   });
+
+submit.addEventListener('click', (e) => {
+  var input = document.getElementById('inputData');
+  var inputValue = input.value;
+  fetch('https://api-nodejs-todolist.herokuapp.com/task', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: 'Bearer ' + sessionStorage.getItem('token'),
+    },
+    body: JSON.stringify({
+      description: inputValue,
+    }),
+  })
+    .then((response) => response.json())
+    .then((response) => {
+      console.log(response);
+    });
+});
